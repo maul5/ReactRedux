c0832d5eb7675109b5a78181cd1aeb77c97d52c3
maul5 <kidipark@gmail.com>
2021-05-26 14:35:04 +0900

    Update store.js
    devtools 추가..

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,4 +14,12 @@ const store = createStore(rootReducer)
 // const middlewares = [logger]
 // const store = createStore(rootReducer, applyMiddleware(...middleware))
 
+// 4_1 (Windows)
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './rootReducer'
+import logger from 'redux-logger'
+const middlewares = [logger]
+const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+const store = createStoreWithMiddleware(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
 export default store
